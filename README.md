7d48ab530bb00ebfa76202a11ef01acb79fb3018
MadKerberos <giuseppe.maddiona@gmail.com>
2020-03-22 12:52:04 +0100

    Duplicate Old Project

--- README.md
+++ README.md
@@ -1,1 +1,77 @@
-# currency-converter
+# Currency Converter
+Online Currency Converter.
+
+**DOCUMENTATION**
+
+The project has been developed using **Node.js. v.10.15.0**. Following the npm packages used:
+-	**express:** to handle HTTP requests
+-	**axios:** used to get XML data from the ECB website
+-	**fast-xml-parser:** used to parse XML and convert into JSON
+-	**lodash:** javascript library used to work easily with objects and array
+-	**nodemon:** very useful during the development phase automatically restarting the node application when file changes are detected.
+
+The api *‘/api/convert’* is called with the GET method and it expects as input an object into the body as the following:
+```
+                                            { 
+                                              "amount" : "10",
+                                              "src_currency" : "EUR",
+                                              "dest_currency" : "USD",
+                                              "reference_date" : "2020-03-20"
+                                            }
+```
+
+The api gets the data from the ECB website as XML format.
+Using the fast-xml-parser, XML data are converted into a JSON object. Then, using the Lodash library, the JSON object is inspected in order to get all information we need such as: list of currencies and conversion rate related to the reference_date.
+
+If one of the parameters is wrong (empty parameter, currency not valid, date not valid or not in the ECB list), the api responds with an error message.
+
+Four cases of currency conversion have been identified:
+
+**Case 0:** both source and destination currencies are equal: the converted amount is equal to the input amount as well as the currency.
+
+**Case 1:** the source currency is equal to EUR. The converted amount is calculated multiplying the conversion rate with the input amount.
+```                                        
+                                          final_amount = source_rate * amount
+```
+**Case 2:** both source and destination currencies are different than EUR. The input amount is converted to EUR and then converted to the destination currency. 
+```
+                                 final_amount = (1/source_rate) * amount * destination_rate
+```
+**Case 3:** the destination currency is equal to EUR. The converted amount is calculated multiplying the amount with the inverse of the conversion rate.
+```
+                                         final_amount = amount * (1/destination_rate)
+```
+
+
+**HOW TO USE THE APP**
+
+Please follow the instructions in order to install and use the app:
+1.	Install **Node.js** on your environment: the version must be equal or greater than 10.15.0;
+2.	Clone the project **currency_converter** locally on your computer: For example, the project path can be “C:\Users\Projects\convert_api";
+3.	Open the command prompt and go to the project folder path "C:\Users\Projects\convert_api". Then, run the server locally digiting 
+```
+                                                    npm run prod
+```
+4.	Install **Postman** on your computer and create a new HTTP request with the following configurations:
+
+    a.	Method: **GET**
+
+    b.	Url request: **localhost/api/convert**
+
+    c.	Body: 
+
+      - select **raw** 
+
+      - select **JSON(application/json)**
+
+      The request body must contain an object as the following:
+
+    ```
+                                              { 
+                                                "amount" : "10",
+                                                "src_currency" : "EUR",
+                                                "dest_currency" : "USD",
+                                                "reference_date" : "2020-03-20"
+                                              }
+    ```
+    Change the body values and click Send: you’ll see on Postman the response to your request.	
---
+++ src/server.js
@@ -0,0 +1,166 @@
+"use strict"
+const express = require('express')
+const app = express()
+const port = 80
+const axios = require('axios');
+
+var bodyParser = require('body-parser'); 
+var xml_parser = require('fast-xml-parser');
+var _ = require('lodash');
+
+
+
+// - - - Avoid conflict with app_request - - - 
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
+
+// Root API
+
+app.get('/api/convert', (req, res) => {
+    
+    var amount = "";
+    var src_currency = "";
+    var dest_currency = "";
+    var reference_date = "";
+    var flag_parameters_ok = false;
+
+    // Fetch value from JSON
+    amount = parseFloat(req.body.amount);
+    src_currency = req.body.src_currency;
+    dest_currency = req.body.dest_currency;
+    reference_date = req.body.reference_date;
+
+    if(amount && src_currency && dest_currency && reference_date ) {
+        flag_parameters_ok = true;
+    }
+    else {
+        res.send("Something is wrong with your parameters");
+        return;
+    }
+    
+    //
+    if(flag_parameters_ok) {
+
+        var final_value = "";
+        var jsonObj = {};
+        var res_json_obj = {};
+
+
+        //Fetch data from the ECB Site, if public API is available you are able to consume your API, else server raise a message error.
+        axios.get('https://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist-90d.xml')
+            .then(response => {
+
+                var cube_currency_list = response.data;
+                        
+                try {
+                    if( xml_parser.validate(cube_currency_list) === true) { 
+
+                        var options = {
+                            ignoreAttributes : false,
+                            trimValues: true
+                        };
+
+                        jsonObj = xml_parser.parse(cube_currency_list,options); //Convert Our XML to JSON.
+                                              
+                        // (1) Find list of currency per Date.
+                        var list_per_date = jsonObj['gesmes:Envelope'].Cube.Cube;
+                        var index_reference_date = _.findIndex(list_per_date, function(o) { return o['@_time'] == reference_date; });
+
+                        if(index_reference_date == -1 ) {
+                            res.send("Date not valid");
+                            return;
+                        }
+
+                        // (2) Find your currencies value.
+                        var currency_list = jsonObj['gesmes:Envelope'].Cube.Cube[index_reference_date].Cube;
+                        var src_currency_index = _.findIndex(currency_list, function(o) { return o['@_currency'] == src_currency; });
+                        var dest_currency_index = _.findIndex(currency_list, function(o) { return o['@_currency'] == dest_currency; });                                            
+                        
+                        //Check if inserted currencies are right.
+                        if( (src_currency != "EUR" && src_currency_index == -1) || ( dest_currency != "EUR" && dest_currency_index == -1)) {
+                            res.send("Currencies not valid");
+                            return;
+                        }
+
+                        //Case 0 - SRC Currency == DST Currency -- Nothing to do
+                        if(src_currency == dest_currency ) {
+                            console.log("Case 0 Called");
+                            res_json_obj.amount = amount;
+                            res_json_obj.currency = dest_currency;
+                            res.send(res_json_obj);
+                            console.log("Case 0 Executed ");
+                            return;
+                        }
+
+                        //Case 1 - SRC Currency == "EUR" && Dest Currency != "EUR"
+                        if(src_currency == "EUR" && dest_currency != "EUR" ) {
+                            console.log("Case 1 Called");
+                            var dest_currency_value = currency_list[dest_currency_index]['@_rate'];
+                            final_value = amount * parseFloat(dest_currency_value);                                                
+
+                            res_json_obj.amount = final_value.toFixed(2);
+                            res_json_obj.currency = dest_currency; 
+                            console.log("Case 1 Executed ");  
+                        }
+
+                        //Case 2 - SRC Currency != EUR && Dest Currency != EUR
+                        if(src_currency != "EUR" && dest_currency != "EUR" ) {
+                            console.log("Case 2 Called");
+                            var src_currency_value = currency_list[src_currency_index]['@_rate'];
+                            var dest_currency_value = currency_list[dest_currency_index]['@_rate'];
+                            final_value = parseFloat(1/src_currency_value) * amount * parseFloat(dest_currency_value);
+
+                            res_json_obj.amount = final_value.toFixed(2);
+                            res_json_obj.currency = dest_currency; 
+                            console.log("Case 2 Executed ");
+                        
+                        }
+
+                        //Case 3 - SRC Currency != EUR && Dest Currency == EUR
+                        if(src_currency != "EUR" && dest_currency == "EUR" ) {
+                            console.log("Case 3 Called");
+                            var src_currency_value = currency_list[src_currency_index]['@_rate'];
+                            final_value = parseFloat(1/src_currency_value) * amount;   
+
+                            res_json_obj.amount = final_value.toFixed(2);
+                            res_json_obj.currency = dest_currency;
+                            console.log("Case 3 Executed ");     
+                        }
+
+                        res.send(res_json_obj);
+                    }
+                }
+                catch (err) {
+                    console.log(err);
+                }
+                
+        })
+        .catch(error => {
+            res.send ("Convert API Not Available");
+        });
+    }
+    
+});
+
+  
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
