ed00a6d0c72412baf83ec7a99a9c93d2d8dddafa
vdolce <48453232+vdolce@users.noreply.github.com>
2020-04-17 22:59:13 +0200

    devServer proxy

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,5 +1,16 @@
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+
+  devServer: {
+    proxy: {
+      "^/api": {
+        target: "http://localhost:8000",
+        changeOrigin: true,
+        logLevel: "debug",
+        pathRewrite: { "^/api": "/" }
+      }
+    }
+  }
 }
