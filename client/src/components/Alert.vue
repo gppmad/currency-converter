683053c39e49f66922fb7c94e03cf60f335ace59
vdolce <48453232+vdolce@users.noreply.github.com>
2020-04-17 23:00:48 +0200

    v-alert component

---
+++ client/src/components/Alert.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container custom-container">
+    <v-alert 
+      class="text-center" 
+      :value="showAlert"
+      :type="alertType" 
+      dismissible
+    >
+      {{alertMessage}}
+    </v-alert>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Alert',
+
+  props:{
+      showAlert: Boolean,
+      alertMessage: String,
+      alertType: String,
+  },
+
+data: () => ({
+
+  }),
+}
+
+</script>
+
+<style scoped>
+
+</style>
